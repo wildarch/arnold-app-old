0d95298c1e444404c0449f510b6a847ce4044e28
lemio <geert.roumen@gmail.com>
2017-12-28 20:52:44 +0100

    Added score system

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@ var config = {
   messagingSenderId: "360543261866"
 };
 firebase.initializeApp(config);
-
+var userId = 0;
 var localList = {"text": []};
 //Select the DOM (HTML) elements
 var todoListDom = document.querySelector("#userList");
@@ -22,14 +22,17 @@ var textDom = document.querySelector("#text");
 //
 var dbRef = firebase.database().ref().child('arnoldApp').child('users');
 
+//var dbRef2 = firebase.database().ref().child('arnoldApp').child('points');
+var ref = firebase.database().ref().child("arnoldApp").child("points");
+
 function drawElement(element,id){
   //This will add a new item to the #todoList
   todoListDom.innerHTML += `
   <div class="item">
     <div class="right floated content">
-      <div class="ui button" onclick="addElement(`+id+`)">+</div>
+      <div class="ui button" onclick="addElement(100,`+id+`)">+</div>
     </div>
-    <img class="ui avatar image" src="` + element.image + `">
+    <img class="ui avatar image" src="images/` + element.image + `">
     <div class="content">
       <a class="header">` + element.name + `</a>
       <div class="description"> (`+element.points+`) Got `+ 100 +` arnold punten from `+ "Fristy"+` </div>
@@ -48,8 +51,14 @@ dbRef.on('value',function (x){
                               localList = x.val();
                               drawList(localList);
                              });
-function addElement(userId){
-  localList[userId].points += 100;
+function addElement(amount,otherId){
+  ref.push({
+    "from": userId,
+    "to": otherId,
+    "amount": amount,
+    "time": Math.floor(Date.now() / 1000)
+  })
+  localList[otherId].points += 100;
   //localList.push({"points":inputName});
   //empty the text element
   //textDom.value = "";
