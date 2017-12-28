c4b1a6f556d4eac88845a67c9ed68befb5954879
Daan de Graaf <daandegraaf9@gmail.com>
2017-12-28 20:09:25 +0100

    Finish login POC

--- public/login/login.js
+++ public/login/login.js
@@ -13,27 +13,48 @@ firebase.initializeApp(config);
 const usersRef = firebase.database().ref().child('arnoldApp').child('users');
 usersRef.on('value', ref => setUsers(ref.val()));
 
-
+const userDom = document.getElementsByClassName("user")[0];
 const usersDom = document.getElementsByClassName('users')[0];
+const userNameDom = userDom.getElementsByClassName("name")[0];
+const userImageDom = userDom.getElementsByTagName('img')[0];
 
-function setUsers(users) {
+function setUser(id) {
+  if(id !== null) {
+    userId = id;
+    localStorage.setItem("userId", userId);
+    const user = users[id];
+    userNameDom.innerText = user.name;
+    userImageDom.setAttribute("src", "/images/" + user.image);
+    usersDom.style["display"] = "none";
+    userDom.style["display"] = "";
+  }
+  else {
+    localStorage.removeItem("userId");
+    userDom.style["display"] = "none";
+    usersDom.style["display"] = "";
+  }
+}
+
+let userId = localStorage.getItem("userId");
+
+let users;
+function setUsers(usrs) {
+  users = usrs;
   usersDom.innerHTML = "";
   for(let i = 0; i < users.length; i++) {
     usersDom.innerHTML += drawUser(i, users[i]);
   }
+  setUser(userId);
 }
 
-function setUser(id) {
-  console.log("Your user is: " + id);
-}
 
 function drawUser(id, user) {
-  return `<div class="ui card">
+  return `<div class="ui card" onclick=setUser(` + id + `)>
     <div class="image">
       <img src="/images/` + user.image + `">
     </div>
     <div class="content">
-      <a class="header" onclick=setUser(` + id + `)>` + user.name + `</a>
+      <a class="header">` + user.name + `</a>
     </div>
   </div>`;
 }
