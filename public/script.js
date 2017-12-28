7f6dbeab47e1e85846556efc6afb7a76d93ccabb
lemio <geert.roumen@gmail.com>
2017-12-28 21:38:09 +0100

    showLast function

--- public/script.js
+++ public/script.js
@@ -12,8 +12,7 @@ firebase.initializeApp(config);
 var userId = 0;
 var localList = {"text": []};
 //Select the DOM (HTML) elements
-var todoListDom = document.querySelector("#userList");
-
+var userListDom = document.querySelector("#userList");
 var textDom = document.querySelector("#text");
 
 /*
@@ -21,13 +20,11 @@ var textDom = document.querySelector("#text");
 */
 //
 var dbRef = firebase.database().ref().child('arnoldApp').child('users');
-
-//var dbRef2 = firebase.database().ref().child('arnoldApp').child('points');
-var ref = firebase.database().ref().child("arnoldApp").child("points");
+var pointsRef = firebase.database().ref().child("arnoldApp").child("points");
 
 function drawElement(element,id){
   //This will add a new item to the #todoList
-  todoListDom.innerHTML += `
+  userListDom.innerHTML += `
   <div class="item">
     <div class="right floated content">
       <div class="ui button" onclick="addElement(100,`+id+`)">+</div>
@@ -40,28 +37,30 @@ function drawElement(element,id){
   </div>`;
   //<div class=\"todoListElement\" id=\"num "+id+"\">"+element.name+"</div>";
 }
-function drawList(list){
+
+function drawList(list,f){
   //This will draw all the elements in the list
-  todoListDom.innerHTML = "";
+  userListDom.innerHTML = "";
   //forEach will loop through all the elements in the array and append execute drawElement(element,index) on them
-  list.forEach(drawElement);
+  list.forEach(f);
 }
 //When the 'text' value changes in firebase
 dbRef.on('value',function (x){
                               localList = x.val();
-                              drawList(localList);
+                              drawList(localList,drawElement);
                              });
 function addElement(amount,otherId){
-  ref.push({
+  pointsRef.push({
     "from": userId,
     "to": otherId,
     "amount": amount,
-    "time": Math.floor(Date.now() / 1000)
+    "time": Math.floor(Date.now() / 1000),
+    "description": "Plopper ontploft"
   })
   localList[otherId].points += 100;
   //localList.push({"points":inputName});
   //empty the text element
   //textDom.value = "";
-  drawList(localList);
+  //drawList(localList);
   dbRef.set(localList);
 }
