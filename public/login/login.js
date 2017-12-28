1a86bcb168c1f0d4ab8087338e0f0c4bda9eb541
Daan de Graaf <daandegraaf9@gmail.com>
2017-12-28 19:32:55 +0100

    Use new profile pictures

--- public/login/login.js
+++ public/login/login.js
@@ -17,18 +17,23 @@ usersRef.on('value', ref => setUsers(ref.val()));
 const usersDom = document.getElementsByClassName('users')[0];
 
 function setUsers(users) {
-  usersDom.innerHTML = users.reduce(
-    (content, user) => content + drawUser(user)
-  , "");
+  usersDom.innerHTML = "";
+  for(let i = 0; i < users.length; i++) {
+    usersDom.innerHTML += drawUser(i, users[i]);
+  }
 }
 
-function drawUser(user) {
+function setUser(id) {
+  console.log("Your user is: " + id);
+}
+
+function drawUser(id, user) {
   return `<div class="ui card">
     <div class="image">
-      <img src="` + user.image + `">
+      <img src="/images/` + user.image + `">
     </div>
     <div class="content">
-      <a class="header">` + user.name + `</a>
+      <a class="header" onclick=setUser(` + id + `)>` + user.name + `</a>
     </div>
   </div>`;
 }
