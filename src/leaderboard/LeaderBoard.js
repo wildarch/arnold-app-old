5ce3b833ddb2466fd03b2fc835181b7f65efe85f
Daan de Graaf <daandegraaf9@gmail.com>
2018-01-06 18:38:59 +0100

    Fix leaderboard display quirks

--- src/leaderboard/LeaderBoard.js
+++ src/leaderboard/LeaderBoard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Image, List, Container } from 'semantic-ui-react';
+import { Header, Grid, Button, Image, List, Container } from 'semantic-ui-react';
 
 export default class LeaderBoard extends Component {
   render() {
@@ -11,18 +11,20 @@ export default class LeaderBoard extends Component {
             .sort((a, b) => b.points - a.points)
             .map(user => (
               <List.Item key={user.index} onClick={(e) => this.props.onSelectArnold(user.index)}>
-               <List.Content floated='right' verticalAlign="middle">
-                 <Button icon="add" content="points" />
-               </List.Content>
-               <Image avatar size="tiny" src={"/images/" + user.image} />
-               <List.Content>
-                 <List.Header>
-                    {user.name}
-                 </List.Header>
-                 <List.Description>
-                   ({user.points})
-                 </List.Description>
-               </List.Content>
+                <Grid verticalAlign='middle' centered>
+                  <Grid.Column width={4}>
+                    <Image size='small' avatar src={"/images/" + user.image} />
+                  </Grid.Column>
+                  <Grid.Column stretched width={8}>
+                    <Header size='tiny'>
+                      {user.name}
+                    </Header>
+                    {user.points} {(user.points === 1) ? 'point' : 'points'}
+                  </Grid.Column>
+                  <Grid.Column verticalAlign='middle' width={4}>
+                    <Button primary icon='plus' />
+                  </Grid.Column>
+                </Grid>
               </List.Item>
             ))
           }
